ef3a9bb00259ffb55a3720cccf59ac3c7716a25b
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-11-04 16:07:03 -0500

    Add necessary files and folders

---
+++ Foursquare-Map-Project/Models/VenuePhotosModel.swift
@@ -0,0 +1,9 @@
+//
+//  VenuePhotosModel.swift
+//  Foursquare-Map-Project
+//
+//  Created by Krystal Campbell on 11/4/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import Foundation
---
+++ Foursquare-Map-Project/Networking/VenueSearchAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  VenueSearchAPIClient.swift
+//  Foursquare-Map-Project
+//
+//  Created by Krystal Campbell on 11/4/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import Foundation
