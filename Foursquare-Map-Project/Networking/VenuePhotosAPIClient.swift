56e2f8a3efcac6b1ed486eecb28264a56d62f254
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-11-04 17:44:55 -0500

    adds json file, and set up networking files with code

--- Foursquare-Map-Project/Networking/VenuePhotosAPIClient.swift
+++ Foursquare-Map-Project/Networking/VenuePhotosAPIClient.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+/*import Foundation
+
+class WeatherAPIClient {
+    
+    private init() {}
+    static let shared = WeatherAPIClient()
+    
+    func getWeather(latLong: String,completionHandler: @escaping (Result<[WeatherData], AppError>)-> Void){
+        
+        let URLString = "https://api.darksky.net/forecast/\(Secrets.key)/\(latLong)"
+        guard let url = URL(string: URLString) else {completionHandler(.failure(AppError.badURL))
+            return
+        }
+    NetworkHelper.manager.performDataTask(withUrl: url, andMethod: .get) { (results)in
+        switch results {
+        case .failure(let error):
+            completionHandler(.failure(error))
+        case .success(let data):
+            do {
+        let weatherDecoded = try JSONDecoder().decode(Weather.self, from: data)
+                completionHandler(.success(weatherDecoded.daily.data))
+            } catch {
+        completionHandler(.failure(AppError.couldNotParseJSON(rawError:error)))
+                        }
+                    }
+                }
+            }
+        }
+*/
+
+
+//https://api.foursquare.com/v2/venues/VENUE_ID/photos
+
+
+//https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD
--- Foursquare-Map-Project/Networking/VenueSearchAPIClient.swift
+++ Foursquare-Map-Project/Networking/VenueSearchAPIClient.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+/*
+import Foundation
+
+class WeatherAPIClient {
+    
+    private init() {}
+    static let shared = WeatherAPIClient()
+    
+    func getWeather(latLong: String,completionHandler: @escaping (Result<[WeatherData], AppError>)-> Void){
+        
+        let URLString = "https://api.darksky.net/forecast/\(Secrets.key)/\(latLong)"
+        guard let url = URL(string: URLString) else {completionHandler(.failure(AppError.badURL))
+            return
+        }
+    NetworkHelper.manager.performDataTask(withUrl: url, andMethod: .get) { (results)in
+        switch results {
+        case .failure(let error):
+            completionHandler(.failure(error))
+        case .success(let data):
+            do {
+        let weatherDecoded = try JSONDecoder().decode(Weather.self, from: data)
+                completionHandler(.success(weatherDecoded.daily.data))
+            } catch {
+        completionHandler(.failure(AppError.couldNotParseJSON(rawError:error)))
+                        }
+                    }
+                }
+            }
+        }
+*/
+
+//"https://api.foursquare.com/v2/venues/search?client_id=\(Secrets.id)&client_secret=\(Secrets.key)&ll=\(latLong)&query=\(query)&v=20191104"
+
+
