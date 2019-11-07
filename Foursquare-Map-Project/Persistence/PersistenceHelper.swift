ab521f08dc31262fcf23053e3bff767d55276de1
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-11-07 17:06:19 -0500

    Adds new files for collection view cell, persistence helper, xcasset icons

---
+++ Foursquare-Map-Project/Persistence/PersistenceHelper.swift
@@ -0,0 +1,9 @@
+//
+//  PersistenceHelper.swift
+//  Foursquare-Map-Project
+//
+//  Created by Krystal Campbell on 11/7/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import Foundation
---
+++ Foursquare-Map-Project/Views/foodCell.swift
@@ -0,0 +1,13 @@
+//
+//  foodCell.swift
+//  Foursquare-Map-Project
+//
+//  Created by Krystal Campbell on 11/6/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import UIKit
+
+class foodCell: UICollectionViewCell {
+    
+}
