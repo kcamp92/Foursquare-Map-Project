2c921c09e16b51c95ee060409308ff6c622f8615
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-11-12 09:36:28 -0500

    add collections cell file to project

---
+++ Foursquare-Map-Project/Views/collectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  collectionCell.swift
+//  Foursquare-Map-Project
+//
+//  Created by Krystal Campbell on 11/12/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import UIKit
+
+class collectionCell: UICollectionViewCell {
+    
+}
