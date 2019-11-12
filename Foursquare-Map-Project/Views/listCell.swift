9e731038a471eb780208563524a9c1c9b5198cb2
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-11-12 18:04:55 -0500

    add new files

---
+++ Foursquare-Map-Project/Views/listCell.swift
@@ -0,0 +1,24 @@
+//
+//  listCell.swift
+//  Foursquare-Map-Project
+//
+//  Created by Krystal Campbell on 11/12/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import UIKit
+
+class listCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
