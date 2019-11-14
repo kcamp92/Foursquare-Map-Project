89049395e662ae64b65ebf02c83ee68a9c929c9a
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-11-14 11:22:07 -0500

    add file to apply color gradients

--- Foursquare-Map-Project/Models/ColorGradients.swift
+++ Foursquare-Map-Project/Models/ColorGradients.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import UIKit
+
+struct Colors {
+    static let shared = Colors()
+    
+func setGradientBackground(colorTop: UIColor, colorBottom: UIColor,newView:UIView) {
+    let gradientLayer = CAGradientLayer()
+    gradientLayer.colors = [colorBottom.cgColor, colorTop.cgColor]
+    gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+    gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
+    gradientLayer.locations = [0, 1]
+    gradientLayer.frame = newView.bounds
+    newView.layer.insertSublayer(gradientLayer, at: 0)
+}
+}
