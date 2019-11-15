751cf4f95126940c8413318538ebd4634b1f9663
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-11-15 17:52:13 -0500

    new files added, createnewcollectionvc, dvc model for annotation segue, model for bookmark collections persistence

---
+++ Foursquare-Map-Project/Models/Bookmark.swift
@@ -0,0 +1,9 @@
+//
+//  Bookmark.swift
+//  Foursquare-Map-Project
+//
+//  Created by Krystal Campbell on 11/15/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import Foundation
---
+++ Foursquare-Map-Project/Models/DVC Model.swift
@@ -0,0 +1,9 @@
+//
+//  DVC Model.swift
+//  Foursquare-Map-Project
+//
+//  Created by Krystal Campbell on 11/15/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import Foundation
