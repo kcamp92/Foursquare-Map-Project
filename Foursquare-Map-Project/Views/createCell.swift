ef8e515d13e05df7dfdc3ecb203b70834048f97b
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-11-18 10:09:48 -0500

    add new file for create collection view cell

---
+++ Foursquare-Map-Project/Views/createCell.swift
@@ -0,0 +1,13 @@
+//
+//  createCell.swift
+//  Foursquare-Map-Project
+//
+//  Created by Krystal Campbell on 11/18/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import UIKit
+
+class createCell: UICollectionViewCell {
+    
+}
